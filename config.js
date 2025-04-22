53e3e40e9d8fc8647d5a42dd16fc9924b3de9967
oscnyd <osnydahl@gmail.com>
2025-04-22 13:36:28 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDrovSRv-f6fFokdBEfwvNMZTqG1qYoU_s",
+  authDomain: "russedaap-f7190.firebaseapp.com",
+  databaseURL: "https://russedaap-f7190-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "russedaap-f7190",
+  storageBucket: "russedaap-f7190.firebasestorage.app",
+  messagingSenderId: "850359212980",
+  appId: "1:850359212980:web:bbc08be108efe1d655f0e1"
+};
